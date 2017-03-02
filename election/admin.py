3f955a21f7ac9b2c5c3ce7dfa8b3a7aaf1aca16d
Master Bruce <vs.ny@gmx.de>
2017-03-02 19:48:51 +0100

    add admin action resize images

--- election/admin.py
+++ election/admin.py
@@ -1,6 +1,23 @@
+import PIL
+from PIL import Image
 from django.contrib import admin
 
 from .models import Ballot, Candidate, SubElection
+
+
+def resize_images(modeladmin, request, queryset):
+    basewidth = 160
+    for election in queryset:
+        for candidate in election.candidate_set.all():
+            path = candidate.img.file.name
+            img = Image.open(path)
+            if basewidth in img.size:
+                print("Already resized")
+                continue
+            wpercent = (basewidth / float(img.size[0]))
+            hsize = int((float(img.size[1]) * float(wpercent)))
+            img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+            img.save(path)
 
 
 class CandidateAdmin(admin.StackedInline):
@@ -13,6 +30,7 @@ class SubElectionAdmin(admin.ModelAdmin):
     inlines = [
         CandidateAdmin,
     ]
+    actions = [resize_images]
 
 
 admin.site.register(Ballot)
