8db711dfd3b54d50efd6a26ee3c3ee18bf7f6d06
VolkerSchiewe <volker.s1994@gmail.com>
2018-11-05 11:10:42 +0100

    fix typo in websocket.ts

--- src/modules/utils/websocket.ts
+++ src/modules/utils/websocket.ts
@@ -2,8 +2,8 @@ import Sockette from "sockette"
 
 export function openWebsocket(endpoint: string, onMessage: (e) => void) {
 
-    const protocol = location.protocol == 'https' ? 'wss' : 'ws';
-    const url = `${protocol}://${location.host}/${endpoint}`;
+    const protocol = location.protocol == 'https:' ? 'wss:' : 'ws:';
+    const url = `${protocol}//${location.host}/${endpoint}`;
 
     const onOpen = () => {
         if (process.env.NODE_ENV !== 'production')
