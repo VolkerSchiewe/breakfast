39a848a9cacbba24fecfb99e9e3e529c2ac13629
VolkerSchiewe <volker.s1994@gmail.com>
2018-11-05 10:28:53 +0100

    fix asgi config

--- breakfast/asgi.py
+++ breakfast/asgi.py
@@ -1,6 +1,12 @@
-import os
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
 
-import channels.asgi
+import os
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "breakfast.settings")
-channel_layer = channels.asgi.get_channel_layer()
+django.setup()
+application = get_default_application()
